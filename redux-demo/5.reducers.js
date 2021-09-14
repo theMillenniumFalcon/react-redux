f3227355c46ab0868f28b423ffe54a0c5b8d07ac
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-14 15:53:27 +0530

    store added

--- redux-demo/5.reducers.js
+++ redux-demo/5.reducers.js
@@ -1,12 +1,11 @@
 const BUY_CAKE = 'BUY_CAKE',
 
 function BuyCake() {
-    {
-    type: BUY_CAKE;
-    info: 'First redux action'
+  return { 
+      type: BUY_CAKE,
+      info: 'First redux action'
+  }
 }
-}
-
 
 // (previousState, action) => newState
 
