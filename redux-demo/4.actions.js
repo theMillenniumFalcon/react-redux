41d7b3a36d89046879e1bf59279b8995ff820eee
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-20 19:24:10 +0530

    comments_added

--- redux-demo/4.actions.js
+++ redux-demo/4.actions.js
@@ -1,5 +1,7 @@
+// first we define a string constant that defines the type of the action
 const BUY_CAKE = 'BUY_CAKE',
 
+// buyCake() is an action creator that returns an action
 function BuyCake() {
   return { 
       type: BUY_CAKE,
