53e9d1cc8106acc9498c5e5bdf598897305aa168
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-11-01 03:31:23 +0530

    ice_cream_reducer_added

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,16 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store} >
       <div className="App">
+        <HooksCakeContainer/>
         <CakeContainer/>
+        <IceCreamContainer/>
       </div>
     </Provider>
   );
--- react-redux-demo/src/redux/cake/cakeReducer.js
+++ react-redux-demo/src/redux/cake/cakeReducer.js
@@ -1,4 +1,4 @@
-import { BUY_CAKE} from './cakeTypes'
+import { BUY_CAKE } from './cakeTypes'
 
 const initialState = {
     numOfCakes: 10
